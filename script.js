251745cc9698e74cdedd534dbc549d843753aa02
D-Joe <development@dobler.email>
2016-03-10 07:29:14 +0100

    Load data from JSON file
    Data for the presentation is loaded from slides.json file, to make it
    easier to change content.

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
-document.onkeydown = checkKey;
-
-var page=-1;
-var revealed=0;
-var hidden=0;
-
-var numberOfSlides=document.getElementsByClassName('slide').length;
-
 var title=document.getElementById('title');
-var slides=document.getElementsByClassName('slide');
 var ending=document.getElementById('ending');
-
+var slides=document.getElementById('slides');
+var slide=document.getElementsByClassName('slide');
 var show=document.getElementById('show');
 
-show.innerHTML=title.innerHTML;
+var numberOfSlides;
+var page=-1;
+
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path,true);
+    httpRequest.send(); 
+}
+
+window.onload = function() {
+	fetchJSONFile('slides.json', function(data){
+		title.innerHTML='<h1>'+data.title+'</h1>'+'<h3>'+data.subtitle+'</h3>';
+		ending.innerHTML='<h1>'+data.end+'</h1>';
+		
+		for (var i = 0; i < Object.keys(data.slides).length; i++)
+		{
+			var points = '';
+			for (var j = 0; j < Object.keys(data.slides[i].points).length; j++)
+			{
+				points = points + '<li>'+data.slides[i].points[j]+'</li>';
+			}
+			slides.innerHTML= slides.innerHTML +
+								'<div class="slide">'+
+									'<h1>'+data.slides[i].slidetitle+'</h1>'+
+									'<ul>'+
+										points +
+									'</ul>'+
+								'</div>';
+		}
+		
+		show.innerHTML=title.innerHTML;
+		numberOfSlides=document.getElementsByClassName('slide').length;
+	});
+}
+document.onkeydown = checkKey;
 
 function checkKey(e) {
 	e = e || window.event;
 	
 	if (e.keyCode == '37') {		//right Key
-		//alert("last page");
 		if(page < 0) {
 			page = -1;
 			changePage(page);
@@ -29,7 +62,6 @@ function checkKey(e) {
 	}
 
 	if (e.keyCode == '39') {		//left Key
-		//alert("next page");
 		if(page >= numberOfSlides -1) {
 			page = numberOfSlides;
 			changePage(page);
@@ -41,23 +73,16 @@ function checkKey(e) {
 	}
 }
 
-
 function changePage(page) {
 	if (page < 0) {
-		//alert("Title");
-		//changeVisibilityByID('show','none');
-		//changeVisibilityByClass('title','block');
+		//Title
 		show.innerHTML=title.innerHTML;
 	} else if (page >= 0 && page <= numberOfSlides - 1) {
-		//alert("Page " + page);
-		//changeVisibilityByClass('title','none');
-		//changeVisibilityByID('ending','none');
-		//changeVisibilityByID('show','block');
-
-		show.innerHTML=slides[page].innerHTML;
+		//Slides
+		show.innerHTML=slide[page].innerHTML;
 
 	} else if (page > numberOfSlides - 1) {
-		//alert("Ending");
+		//Ending
 		show.innerHTML=ending.innerHTML;
 	}
 }
